7904f9066a63e262c836e9a5cd9578b7b4eb3060
Gonzalo Campos <erick.campos@wizeline.com>
2019-11-04 21:52:09 -0600

    Creating our first assertion

--- src/test/kotlin/chapter/one/soundex/SoundexTest.kt
+++ src/test/kotlin/chapter/one/soundex/SoundexTest.kt
@@ -1,5 +1,6 @@
 package chapter.one.soundex
 
+import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
@@ -13,5 +14,7 @@ class SoundexTest {
         val soundex: Soundex = Soundex()
 
         val encoded = soundex.encode("A")
+
+        assertEquals(encoded, "A")
     }
 }
