d44622ccb914dd2489aeb51cff2db6b571b81267
Gonzalo Campos <erick.campos@wizeline.com>
2019-11-04 21:21:10 -0600

    Adds failing test.

---
+++ src/test/kotlin/chapter/one/soundex/SoundexTest.kt
@@ -0,0 +1,16 @@
+package chapter.one.soundex
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class SoundexTest {
+
+
+    @Test
+    fun `retains Sole Letter Of One Letter Word`() {
+        val soundex: Soundex? = Soundex()
+
+    }
+}
