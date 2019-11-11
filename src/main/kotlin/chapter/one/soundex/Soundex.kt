76fe787e332b79dd1df42676a9601b1fe8f6c423
Gonzalo Campos <erick.campos@wizeline.com>
2019-11-10 20:38:05 -0600

    Refactored code

--- src/main/kotlin/chapter/one/soundex/Soundex.kt
+++ src/main/kotlin/chapter/one/soundex/Soundex.kt
@@ -4,35 +4,40 @@ class Soundex {
 
     private val MAX_CODE_LENGTH = 4
 
-    fun encode(word: String) : String {
-        return zeroPad(head(word) + encodedDigits(tail(word)) )
+    fun encode(word: String): String {
+        return zeroPad(head(word) + encodedDigits(tail(word)))
     }
 
-    private fun head(word: String) : String {
+    private fun head(word: String): String {
         return word.substring(0, 1)
     }
 
-    private fun encodedDigits(word: String) : String =
-        if(word.isNotEmpty()) encodedDigit(word[0]) else ""
+    private fun encodedDigits(word: String): String =
+        buildString {
+            for (i in word) {
+                append(encodedDigit(i))
+            }
+        }
 
-    private fun encodedDigit(letter: Char) : String {
+    private fun encodedDigit(letter: Char): String {
         val encodings = hashMapOf(
             'b' to "1", 'f' to "1", 'p' to "1", 'v' to "1",
             'c' to "2", 'g' to "2", 'j' to "2", 'k' to "2", 'q' to "2", 's' to "2", 'x' to "2", 'z' to "2",
             'd' to "3", 't' to "3",
             'l' to "4",
             'm' to "5", 'n' to "5",
-            'r' to "6")
+            'r' to "6"
+        )
 
         val encode = encodings[letter]
         return encode ?: ""
     }
 
-    private fun zeroPad(word: String) : String {
+    private fun zeroPad(word: String): String {
         val zerosNeeded = MAX_CODE_LENGTH - word.length
         return word + "0".repeat(zerosNeeded)
     }
 
-    private fun tail(word: String) : String =
+    private fun tail(word: String): String =
         word.substring(1)
 }
